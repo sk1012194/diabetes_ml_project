90f103d889c73db335009e28fb92a6b24c26c06e
sk1012194 <127843333+sk1012194@users.noreply.github.com>
2023-06-13 22:42:25 +0530

    Update diabetes_prediction.py

--- diabetes_prediction.py
+++ diabetes_prediction.py
@@ -26,7 +26,7 @@ def diabetes_prediction(input_data):
  prediction = loaded_model.predict(input_data_reshaped)
  print(prediction)
 
- if (prediction[0] == 0):
+ if (prediction[0] == 1):
    return'The person is not diabetic'
  else:
    return'The person is diabetic'
